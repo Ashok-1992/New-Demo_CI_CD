fde6fd4a1a21f47ba3ac85c739bd866a08f621c4
Ashok-1992 <ashokchan92@gmail.com>
2026-05-06 05:59:38 +0530

    Remove unnecessary newline in train.py

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 print("ML pipeline running in GitHub Actions")
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
-
 X, y = load_iris(return_X_y=True)
 
 model = RandomForestClassifier()
